17a4f8ed99317e7392477a7a75f44565e01d1c91
youssef-ismail <youssefismail147@gmail.com>
2026-08-06 16:11:02 +0300

    chore: update EF Core migrations

---
+++ HomePal.Persistence/Migrations/20260806131045_AddPantryAndPantryItemEntities.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomePal.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPantryAndPantryItemEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pantries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HouseholdId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pantries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pantries_Households_HouseholdId",
+                        column: x => x.HouseholdId,
+                        principalTable: "Households",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PantryItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PantryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ExpireDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Quantity = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    MeasuringUnitId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CategoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PantryItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PantryItems_MeasuringUnits_MeasuringUnitId",
+                        column: x => x.MeasuringUnitId,
+                        principalTable: "MeasuringUnits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PantryItems_Pantries_PantryId",
+                        column: x => x.PantryId,
+                        principalTable: "Pantries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PantryItems_ProductCategories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "ProductCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pantries_HouseholdId",
+                table: "Pantries",
+                column: "HouseholdId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PantryItems_CategoryId",
+                table: "PantryItems",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PantryItems_MeasuringUnitId",
+                table: "PantryItems",
+                column: "MeasuringUnitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PantryItems_PantryId",
+                table: "PantryItems",
+                column: "PantryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PantryItems");
+
+            migrationBuilder.DropTable(
+                name: "Pantries");
+        }
+    }
+}
